7ae8b2aa48a6ef1a663e7fc83614ce771b15229b
luislaya <luislayawen@gmail.com>
2023-11-02 11:29:41 -0500

    Solution

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote[stock], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +17,35 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote[stock], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+def test_getRatio_priceBZero(self):
+     price_a = 107.2
+     price_b = 0
+     self.assertIsNone(getRatio(price_a, price_b),0)
+  
+def test_getRatio_priceAZero(self):
+     price_a = 0
+     price_b = 111.68
+     self.assertIsNone(getRatio(price_a, price_b),0)
+  
+def test_getRatio_greaterThan1(self):
+     price_a = 360.40
+     price_b = 177.23
+     self.assertIsNone(getRatio(price_a, price_b), 1)
+  
+def test_getRatio_LessThan1(self):
+     price_a = 177.23
+     price_b = 360.40
+     self.assertIsNone(getRatio(price_a, price_b), 1)
 
+def test_getRatio_exactlyOne(self):
+     price_a = 200.21
+     price_b = 200.21
+     self.assertIsNone(getRatio(price_a, price_b),1)
 
 
 if __name__ == '__main__':
